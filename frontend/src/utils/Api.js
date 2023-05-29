c1aa40b7f120ac56b1f554ef2add3cd71e6ec3ed
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-05-30 00:09:30 +0300

    Test gitpush.bat no.2

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -97,8 +97,8 @@ class Api {
 const api = new Api({
   baseUrl: "https://api.milinova.nomoredomains.rocks/",
   headers: {
-    //    authorization: "4efae440-5715-4ca9-8417-962742ac588e",
     "Content-Type": "application/json",
+    //    authorization: "4efae440-5715-4ca9-8417-962742ac588e",
   },
 })
 
